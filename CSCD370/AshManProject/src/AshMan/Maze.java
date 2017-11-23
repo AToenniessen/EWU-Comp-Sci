3949aa0ac3c277016d0fb357eebb89f02056795a
AToenniessen <ajt110195@gmail.com>
2017-11-23 08:40:00 -0800

    Game is drawn, array represents random starting position of ghosts/ashman

--- CSCD370/AshManProject/src/AshMan/Maze.java
+++ CSCD370/AshManProject/src/AshMan/Maze.java
@@ -5,6 +5,12 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 class Maze {
+    private final int empty = 0;
+    private final int wall = 1;
+    private final int cake = 2;
+    private final int ghost = 3;
+    private final int player = 4;
+    private final int imageWidth = 26;
     private int [][] mGame = {
             {2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,1,2,1,2,2},
             {2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2},
@@ -36,21 +42,34 @@ class Maze {
         GraphicsContext gc = mCanvas.getGraphicsContext2D();
         for(int r = 0; r < mGame.length; r++){
             for(int c = 0; c < mGame.length; c++){
-                if(mGame[r][c] == 1){
-                    gc.drawImage(new Image("Images/Wall.png"), r * 26, c * 26);
+                if(mGame[r][c] == wall){
+                    gc.drawImage(new Image("Images/Wall.png"), r * imageWidth, c * imageWidth);
                 }
-                if(mGame[r][c] == 2){
-                    gc.drawImage(new Image("Images/Cake.png"), r * 26, c * 26);
+                else if(mGame[r][c] == cake){
+                    gc.drawImage(new Image("Images/Cake.png"), r * imageWidth, c * imageWidth);
+                }
+                else if(mGame[r][c] == ghost){
+                    gc.drawImage(new Image("Images/Ghost.png"), r * imageWidth, c * imageWidth);
+                }
+                else if(mGame[r][c] == player){
+                    gc.drawImage(new Image("Images/AshMan-Closed.png"), r * imageWidth, c * imageWidth);
                 }
             }
         }
     }
     private void addGhost(int n){
-        while(n != 0){
+        while(n >= 0){
             int r = (int)(Math.random() * 20);
             int c = (int)(Math.random() * 20);
             if(mGame[r][c] == 2){
-                //create ghost objects and ashman Object
+                if(n > 0) {
+                    mGame[r][c] = 3;
+                    n--;
+                }
+                else{
+                    mGame[r][c] = 4;
+                    n--;
+                }
             }
         }
     }
