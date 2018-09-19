aff6de46e3db47bd48eb61565766178e5c4785f3
AToenniessen <atoenniessen@eagles.ewu.edu>
2018-09-19 14:24:41 -0700

    Test Commit

---
+++ CSCD340/labs/Lab1/project/genericArray/genericArray.c
@@ -0,0 +1,26 @@
+#include "genericArray.h"
+
+
+void printArray(GenericArray * array, int length, void (*printType)(void *))
+{
+	int x;
+	printf("\n");
+	for(x = 0; x < length; x++)
+		printType(array[x].data);
+	
+	printf("\n");
+}
+
+
+void cleanArray(GenericArray * array, int length, void (*cleanType)(void *))
+{
+	int x;
+	for(x = 0; x < length; x++)
+		cleanType(array[x].data);
+	
+	free(array);
+	array = NULL;
+
+}
+
+
---
+++ CSCD340/labs/Lab1/project/utils/fileUtils.c
@@ -0,0 +1,2 @@
+#include "fileUtils.h"
+
