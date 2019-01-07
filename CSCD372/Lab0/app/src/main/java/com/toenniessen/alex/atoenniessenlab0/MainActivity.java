f1e4318736ee8396e927f5a6f30e5e9638ae69b8
AToenniessen <atoenniessen@eagles.ewu.edu>
2019-01-07 11:42:40 -0800

    Initial commit

---
+++ CSCD372/Lab0/app/src/main/java/com/toenniessen/alex/atoenniessenlab0/MainActivity.java
@@ -0,0 +1,13 @@
+package com.toenniessen.alex.atoenniessenlab0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
