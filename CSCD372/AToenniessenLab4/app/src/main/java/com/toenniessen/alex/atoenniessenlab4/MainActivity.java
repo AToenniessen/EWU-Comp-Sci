b3a41d0e66265d7d3b152d1c8bbbfa6cd2fac74b
AToenniessen <atoenniessen@eagles.ewu.edu>
2019-02-02 18:53:19 -0800

    - Implemented Lab 0 refactor - modified about_data to report "Lab 4" @todo - implement seven segment class that draws at least one segment for display - finish task 1

---
+++ CSCD372/AToenniessenLab4/app/src/main/java/com/toenniessen/alex/atoenniessenlab4/MainActivity.java
@@ -0,0 +1,40 @@
+package com.toenniessen.alex.atoenniessenlab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.toenniessen.alex.atoenniessenlab4.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// This adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Toast.makeText(this,getResources().getString(R.string.about_data),
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onToggleClick(View view) {
+
+    }
+}
