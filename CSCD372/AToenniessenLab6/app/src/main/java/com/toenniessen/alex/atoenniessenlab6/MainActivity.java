652946a06d145e6e9c2578fc01b98066d78386c8
AToenniessen <atoenniessen@eagles.ewu.edu>
2019-02-17 20:52:41 -0800

    - Lab 3 refactor (not lab 0) - Implemented more concise file parsing that fixed missing manufacturer - Implemented crude margin fix to not place label over list drop down arrow
    @TODO
    - Task 2

--- CSCD372/AToenniessenLab6/app/src/main/java/com/toenniessen/alex/atoenniessenlab6/MainActivity.java
+++ CSCD372/AToenniessenLab6/app/src/main/java/com/toenniessen/alex/atoenniessenlab6/MainActivity.java
@@ -1,13 +1,100 @@
 package com.toenniessen.alex.atoenniessenlab6;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ExpandableListView.OnChildClickListener, Serializable {
+    private ArrayList<Manufacturer> mManufacturer = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExpandableListView listView = findViewById(R.id.customList);
+        listView.setOnChildClickListener(this);
+        MyListAdapter adapter = null;
+
+        if(savedInstanceState == null) {
+            if (parseFile("input.txt")) {
+                adapter = new MyListAdapter(this, mManufacturer);
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.parse_fail),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            mManufacturer = (ArrayList<Manufacturer>) savedInstanceState.getSerializable("Manufacturer");
+            adapter = new MyListAdapter(this, mManufacturer);
+        }
+        listView.setAdapter(adapter);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// This adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Toast.makeText(this, getResources().getString(R.string.about_data),
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putSerializable("Manufacturer", mManufacturer);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    private boolean parseFile(String fname){
+        AssetManager manager = getResources().getAssets();
+        BufferedReader input;
+        try {
+            input = new BufferedReader(new InputStreamReader(manager.open(fname)));
+            while(input.ready()){
+                ArrayList<String> lines = new ArrayList<>(Arrays.asList(input.readLine().split(",")));
+                String manufacturer = lines.remove(0);
+                mManufacturer.add(new Manufacturer(manufacturer, new ArrayList<>(lines)));
+            }
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        Manufacturer temp = mManufacturer.get(groupPosition);
+        if(temp != null){
+            Toast.makeText(this, "Manufacturer: " + temp.getManufacturer() +
+                            "\nModel: " + temp.getModel(childPosition),
+                    Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
---
+++ CSCD372/AToenniessenLab6/app/src/main/java/com/toenniessen/alex/atoenniessenlab6/MyListAdapter.java
@@ -0,0 +1,138 @@
+package com.toenniessen.alex.atoenniessenlab6;
+
+import android.app.Activity;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyListAdapter extends BaseExpandableListAdapter implements View.OnClickListener{
+    private Activity activity;
+    private ArrayList<Manufacturer> manufacturers;
+
+    MyListAdapter(Activity act, ArrayList<Manufacturer> manufacturerArrayList) {
+        activity = act;
+        manufacturers = manufacturerArrayList;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return manufacturers.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        if (checkGroup(groupPosition))
+            return manufacturers.get(groupPosition).modelCount();
+        return -1;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        if (checkGroup(groupPosition))
+            return manufacturers.get(groupPosition);
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        if (checkGroup(groupPosition)) {
+            Manufacturer temp = manufacturers.get(groupPosition);
+            if (checkChild(groupPosition, childPosition)) {
+                return temp.getModel(childPosition);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        if (checkGroup(groupPosition))
+            return groupPosition;
+        return -1;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        if (checkGroup(groupPosition)) {
+            if(checkChild(groupPosition, childPosition)){
+                return childPosition;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater layout = activity.getLayoutInflater();
+            convertView = layout.inflate(R.layout.manufacturer, null);
+        }
+        TextView temp = (TextView) convertView.findViewById(R.id.manufacturer);
+        Manufacturer manufacturer = (Manufacturer) getGroup(groupPosition);
+        temp.setText(manufacturer.getManufacturer());
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater layout = activity.getLayoutInflater();
+            convertView = layout.inflate(R.layout.model, null);
+        }
+
+        TextView temp = (TextView) convertView.findViewById(R.id.model);
+        temp.setText((String) getChild(groupPosition, childPosition));
+        ImageView delete = (ImageView) convertView.findViewById(R.id.delete);
+        delete.setTag(R.id.group_num, groupPosition);
+        delete.setTag(R.id.posn_num, childPosition);
+        delete.setOnClickListener(this);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    private boolean checkGroup(int groupPos) {
+        return (groupPos > -1 && groupPos < manufacturers.size());
+    }
+
+    private boolean checkChild(int groupPos, int childPos) {
+        Manufacturer temp = manufacturers.get(groupPos);
+        return (childPos > -1 && childPos < temp.modelCount());
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int group = (int) v.getTag(R.id.group_num);
+        final int child = (int) v.getTag(R.id.posn_num);
+
+        Snackbar snackbar = Snackbar.make(v, "Are you sure you want to delete?", Snackbar.LENGTH_LONG);
+        snackbar.setAction("CONFIRM", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Manufacturer manufacturer = (Manufacturer) getGroup(group);
+                        manufacturer.removeModel(manufacturer.getModel(child));
+                        notifyDataSetChanged();
+                    }
+                });
+
+        snackbar.show();
+
+
+    }
+}
