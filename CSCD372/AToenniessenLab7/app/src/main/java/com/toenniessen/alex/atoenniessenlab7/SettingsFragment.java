f7a0c1da3d67e2bf1c8a3f664ebdfef7664f96ef
AToenniessen <atoenniessen@eagles.ewu.edu>
2019-02-26 21:14:55 -0800

    - Implemented summary changes, ListPreferences cause crash when attempting to change summary value for unknown reason
    @TODO
    - Task 3 part 1-2
    - Fix bug that causes crash

--- CSCD372/AToenniessenLab7/app/src/main/java/com/toenniessen/alex/atoenniessenlab7/SettingsFragment.java
+++ CSCD372/AToenniessenLab7/app/src/main/java/com/toenniessen/alex/atoenniessenlab7/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.toenniessen.alex.atoenniessenlab7;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,6 +26,11 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+//        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), getResources().getStringArray(R.array.clock_types)[0]); //crashes because of getpreferencescreen
+//        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), getResources().getStringArray(R.array.clock_types)[1]);
+//        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), getResources().getStringArray(R.array.clock_types)[2]);
+//        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), getResources().getStringArray(R.array.clock_types)[3]);
+
     }
 
     @Override
@@ -41,6 +47,8 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
 
+        preference.setSummary(key);
     }
 }
